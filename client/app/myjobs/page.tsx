b3b8571fef0d52b37f52fe1d80c84bba7abd1225
Ashan Kasthuriarachchi <145893442+ashan2001ruh@users.noreply.github.com>
2025-03-07 01:04:42 +0530

    create homepage and login

---
+++ client/app/myjobs/page.tsx
@@ -0,0 +1,12 @@
+"use Client";
+import React from 'react'
+import Header from '@/Components/Header'
+
+function page() {
+
+      return <div>
+        <Header />
+      </div>;
+    };
+
+export default page;
