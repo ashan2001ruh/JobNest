6316e8d75a8d70f416b3ec7871deaa3e0d5d26e4
Ashan Kasthuriarachchi <145893442+ashan2001ruh@users.noreply.github.com>
2025-03-08 13:45:38 +0530

    create required parts to view and apply jobs

--- client/app/myjobs/page.tsx
+++ client/app/myjobs/page.tsx
@@ -1,12 +1,87 @@
-
+"use client";
 import React from 'react'
 import Header from '@/Components/Header'
+import { useGlobalContext } from "@/context/globalContext";
+import { useJobsContext } from "@/context/jobsContext";
+import { useRouter } from "next/navigation";
+import { Job } from '@/types/type';
+import MyJob from '@/Components/JobItem/MyJob';
+import Footer from '@/Components/Footer';
 
 function page() {
+  const { userJobs, jobs } = useJobsContext();
+  const { isAuthenticated, loading, userProfile } = useGlobalContext();
+  const [activeTab, setActiveTab] = React.useState("posts");
+  const userId = userProfile?._id;
+  const router = useRouter();
+  
+  //Redirect to login if user is not authenticated
+  const likedJobs = jobs.filter((job: Job) => {
+    return job.applicants.includes(userId);
+  });
 
-      return <div>
-        <Header />
-      </div>;
-    };
+  
+  if (loading) {
+    return null;
+  }
+
+  console.log("User Profile:", userProfile);
+
+
+  return (
+    <div>
+      <Header />
+
+      <div className="mt-8 w-[90%] mx-auto flex flex-col">
+        <div className="self-center flex items-center gap-6">
+          <button
+            className={`border border-gray-400 px-8 py-2 rounded-full font-medium
+          ${
+            activeTab === "posts"
+              ? "border-transparent bg-[#7263F3] text-white"
+              : "border-gray-400"
+          }`}
+            onClick={() => setActiveTab("posts")}
+          >
+            My Job Posts
+          </button>
+          <button
+            className={`border border-gray-400 px-8 py-2 rounded-full font-medium
+          ${
+            activeTab === "likes"
+              ? "border-transparent bg-[#7263F3] text-white"
+              : "border-gray-400"
+          }`}
+            onClick={() => setActiveTab("likes")}
+          >
+            Liked Jobs
+          </button>
+        </div>
+
+        {activeTab === "posts" && userJobs.length === 0 && (
+          <div className="mt-8 flex items-center">
+            <p className="text-2xl font-bold">No job posts found.</p>
+          </div>
+        )}
+
+        {activeTab === "likes" && likedJobs.length === 0 && (
+          <div className="mt-8 flex items-center">
+            <p className="text-2xl font-bold">No liked jobs found.</p>
+          </div>
+        )}
+
+        <div className="my-8 grid grid-cols-2 gap-6">
+          {activeTab === "posts" &&
+            userJobs.map((job: Job) => <MyJob key={job._id} job={job} />)}
+
+          {activeTab === "likes" &&
+            likedJobs.map((job: Job) => <MyJob key={job._id} job={job} />)}
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
 
 export default page;
--- client/context/jobsContext.js
+++ client/context/jobsContext.js
@@ -116,8 +116,12 @@ export const JobsContextProvider = ({ children }) => {
 
      // apply to a job
     const applyToJob = async (jobId) => {
+      const job = jobs.find(job => job._id === jobId);
 
-
+        if(job && job.applicants.includes(userProfile._id)){
+          toast.error("You have already applied to this job");
+          return;
+        }
         try {
         const res = axios.put(`/api/v1/jobs/apply/${jobId}`);
 
